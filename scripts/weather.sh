9b1a015eb0e90276a247798ebea80ccbac0370a9
Joakim Calais <joakim.calais@gmail.com>
2018-06-14 22:18:48 +0300

    Fixed template for offline temperature sensors.

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-wget -O /home/pi/web/hue/varsel.xml http://www.yr.no/place/Finland/Southern_Finland/Vantaa/varsel.xml
-
+wget -O /home/pi/web/hue/varsel_tmp.xml http://www.yr.no/place/Finland/Southern_Finland/Vantaa/varsel.xml
+if [ -f "/home/pi/web/hue/varsel_tmp.xml" ]; then
+  cp /home/pi/web/hue/varsel_tmp.xml /home/pi/web/hue/varsel.xml
+fi
